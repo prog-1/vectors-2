1bd539f160e0c13d6aa29376027069ef32bf5325
vay4ie <vay4ie@gmail.com>
2023-02-09 08:23:54 +0200

    Added t2 and segment restriction

--- intersection.go
+++ intersection.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"image/color"
 	"log"
 
@@ -22,7 +23,7 @@ type Game struct {
 }
 
 type vector struct {
-	x, y int
+	x, y float64
 }
 
 type line struct {
@@ -45,22 +46,32 @@ func (g *Game) Update() error {
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	//Line Draw
-	ebitenutil.DrawLine(screen, fl((g.width/2)+g.l1.a.x), fl((g.height/2)+g.l1.a.y), fl((g.width/2)+g.l1.b.x), fl((g.height/2)+g.l1.b.y), g.l1.clr)
-	ebitenutil.DrawLine(screen, fl((g.width/2)+g.l2.a.x), fl((g.height/2)+g.l2.a.y), fl((g.width/2)+g.l2.b.x), fl((g.height/2)+g.l2.b.y), g.l2.clr)
-	p := intersectionPoint(g.l1, g.l2)
-	//fmt.Println(p)
-	ebitenutil.DrawCircle(screen, fl((g.width/2)+p.x), fl((g.height/2)+p.y), fl(g.c.r), g.c.clr)
+	ebitenutil.DrawLine(screen, fl(g.width/2)+g.l1.a.x, fl(g.height/2)+g.l1.a.y, fl(g.width/2)+g.l1.b.x, fl(g.height/2)+g.l1.b.y, g.l1.clr)
+	ebitenutil.DrawLine(screen, fl(g.width/2)+g.l2.a.x, fl(g.height/2)+g.l2.a.y, fl(g.width/2)+g.l2.b.x, fl(g.height/2)+g.l2.b.y, g.l2.clr)
+	p, draw := intersectionPoint(g.l1, g.l2)
+	fmt.Println(p)
+	if draw {
+		ebitenutil.DrawCircle(screen, fl(g.width/2)+p.x, fl(g.height/2)+p.y, fl(g.c.r), g.c.clr)
+	}
 }
 
 //-------------------------Functions----------------------------------
 
-func intersectionPoint(l1, l2 line) (p vector) {
+func intersectionPoint(l1, l2 line) (p vector, draw bool) {
 	a, v, b, u := l1.a, l1.b, l2.a, l2.b
 
 	t1 := (u.x*(a.y-b.y) + u.y*(b.x-a.x)) / (u.y*v.x - u.x*v.y)
-	p.x = a.x + v.x*t1
-	p.y = a.y + v.y*t1
-	return p
+	t2 := (v.x*(a.y-b.y) + v.y*(b.x-a.x)) / (u.y*v.x - u.x*v.y)
+	fmt.Println(t1)
+	p.x = a.x + t1*(v.x-a.x)
+	p.y = a.y + t1*(v.y-a.y)
+
+	//if 0<=t1<=1 and 0 <=t2<=1 => true, draw the circle
+	return p, inRange(t1) && inRange(t2)
+}
+
+func inRange(v float64) bool {
+	return 0 <= v && v <= 1
 }
 
 func fl(v int) float64 { return float64(v) }
@@ -72,7 +83,7 @@ func (g *Game) Layout(inWidth, inHeight int) (outWidth, outHeight int) {
 }
 
 func main() {
-	ebiten.SetWindowSize(sW, sH)
+	ebiten.SetWindowSize(640, 480)
 	ebiten.SetWindowTitle("Intersextion Point")
 	ebiten.SetWindowResizable(true) //enablening window resizes
 
@@ -80,7 +91,9 @@ func main() {
 	g := &Game{width: sW, height: sH,
 		l1: line{vector{100, 50}, vector{-100, -50}, color.RGBA{255, 100, 100, 255}},
 		l2: line{vector{-100, 100}, vector{100, -100}, color.RGBA{100, 255, 100, 255}},
-		c:  circle{r: 5, clr: color.RGBA{100, 100, 255, 255}}}
+		//l1: line{vector{20, 40}, vector{40, 10}, color.RGBA{255, 100, 100, 255}},
+		//l2: line{vector{20, 10}, vector{40, 20}, color.RGBA{100, 255, 100, 255}},
+		c: circle{r: 5, clr: color.RGBA{100, 100, 255, 255}}}
 
 	//running game
 	if err := ebiten.RunGame(g); err != nil {
