a30b9b41bd4204f64ee73d550acfcba9421fd09b
Aleksandr Stukalov <stukalovsaleksandrs@gmail.com>
2023-02-20 00:28:15 +0200

    Implemented everything, but it doesn't work)

--- main_test.go
+++ main_test.go
@@ -4,17 +4,23 @@ import "testing"
 
 func TestIncludesPoint(t *testing.T) {
 	for _, tc := range []struct {
-		l1, l2 Line
-		want   Vector
+		l1, l2    line
+		want      vector
+		intersect bool
 	}{
-		// {Line{Vector{2, 4}, Vector{3, 1}}, Line{Vector{3, 1}, Vector{2, 2}}, Vector{8, 6}},
-		// {Line{Vector{1, 1}, Vector{1, 2}}, Line{Vector{1, 3}, Vector{1, 1}}, Vector{3, 5}},
-		{Line{Vector{5, 3}, Vector{6, 8}}, Line{Vector{3, 5}, Vector{10, 4}}, Vector{8, 7}},
+		{line{vector{-5, -2}, vector{4, 7}}, line{vector{7, 2}, vector{-7, 4}}, vector{0, 3}, true},
 	} {
-		if got := IntersectionPoint(&tc.l1, &tc.l2); got.x != tc.want.x || got.y != tc.want.y {
+		got, intersect := IntersectionPoint(&tc.l1, &tc.l2)
+		if tc.intersect != intersect {
+			switch intersect {
+			case true:
+				t.Errorf("Lines intersect when they must not")
+			case false:
+				t.Errorf("Lines are parallel when they must intersect")
+			}
+		}
+		if got.x != tc.want.x || got.y != tc.want.y {
 			t.Errorf("got = %v, want = %v", got, tc.want)
-		} else {
-			t.Errorf("Not right, wrong)")
 		}
 
 	}
